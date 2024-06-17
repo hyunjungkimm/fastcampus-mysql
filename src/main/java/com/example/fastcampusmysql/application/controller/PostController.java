c2b8d877d65fad84e23f8ea3546a0ca6ae4af222
kimhyeonjeong <khjjung1124@gmail.com>
2024-06-17 15:44:02 +0900

    [240617] mysql - 내가 쓴 캘린더 구현

---
+++ src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
@@ -0,0 +1,30 @@
+package com.example.fastcampusmysql.application.controller;
+
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
+import com.example.fastcampusmysql.domain.post.dto.PostCommand;
+import com.example.fastcampusmysql.domain.post.service.PostReadService;
+import com.example.fastcampusmysql.domain.post.service.PostWriteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostWriteService postWriteService;
+    private final PostReadService postReadService;
+
+    @PostMapping("")
+    public Long create(PostCommand postCommand) {
+        return postWriteService.create(postCommand);
+    }
+
+    @GetMapping("/daily-post-counts")
+    public List<DailyPostCount> getDailyPostCounts(DailyPostCountRequest request) {
+        return postReadService.getDailyPostCount(request);
+    }
+}
